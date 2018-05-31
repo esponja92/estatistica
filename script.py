b09d953e963f51741cf78b7532bf279459ea631f
Hugo Dantas <hugobcmt@gmail.com>
2018-05-31 18:29:55 -0300

    melhorando os outputs

--- script.py
+++ script.py
@@ -9,13 +9,13 @@ TESTE DAS HIPÓTESES (WIKIPEDIA):
 3) Afundado propositadamente
 
 "Alguns teóricos da conspiração acreditam que o Titanic foi afundado de propósito para eliminar a oposição à criação do Banco da Reserva Federal. Alguns dos homens mais ricos do mundo estavam a bordo do Titanic para sua viagem inaugural, vários dos quais, incluindo John Jacob Astor IV, Benjamin Guggenheim e Isidor Straus, eram supostamente contra à criação de um banco central americano. Todos os três homens morreram durante o naufrágio."
-Nesse caso, e partindo do princípio que o plano foi bem sucedido, são informações importantes para a predição da sobrevivência "Pclass", "Sex_cleaned", "Fare" e "Embarked_cleaned", pois são as características importantes para demarcar dos "homens mais ricos do mundo". A performance média deste modelo é dada por:
+Nesse caso, e partindo do princípio que o plano foi bem sucedido, são informações importantes para a predição da sobrevivência "Pclass", "Sex_cleaned" e "Fare", pois são as características importantes para demarcar os "homens mais ricos do mundo". A performance média deste modelo é dada por:
 
-Media:  77.4528037383
-de:  74.9522142803
-ate:  79.9533931963
+Media:  76.9088785047
+de:  74.5287826871
+ate:  79.2889743223
 
-Além disso, este modelo classifica os nomes citados como sobreviventes. Logo, esta teoria é estatísticamente improvável
+Além disso, este modelo classifica os nomes citados como sobreviventes. Logo, por absurdo, esta teoria é estatísticamente improvável
 
 4) Portas estanques fechadas
 5) Teoria das juntas de expansão
@@ -38,9 +38,9 @@ used_features =[
     "Parch"
 ]
 
-Media:  78.1654205607
-de:  75.6607771309
-ate:  80.6700639906
+Media:  78.3836448598
+de:  75.9748618092
+ate:  80.7924279104
 
 
 '''
@@ -77,11 +77,11 @@ data=data[[
 ]].dropna(axis=0, how='any')
 
 used_features =[
-    "Pclass",
-#    "Sex_cleaned",
+#    "Pclass",
+    "Sex_cleaned",
 #    "Age",
-#    "SibSp",
-#    "Parch",
+    "SibSp",
+    "Parch",
 #    "Fare",
 #    "Embarked_cleaned"
 ]
@@ -92,6 +92,7 @@ gnb = GaussianNB()
 
 x = []
 
+print "Calculando as performances do modelo...\n"
 for i in range(1000):
 
     # Split dataset in training and test datasets
@@ -106,7 +107,6 @@ for i in range(1000):
     y_pred = gnb.predict(X_test[used_features])
 
     x.append(float(100*(1-(X_test["Survived"] != y_pred).sum()/float(X_test.shape[0]))))
-    print i+1
 
 x.sort()
 mean = np.mean(x)
@@ -132,5 +132,12 @@ data["Embarked_cleaned"]=np.where(data["Embarked"]=="S",0,
                                            np.where(data["Embarked"]=="Q",2,3)
                                           )
                                  )
+print "\n---------- TESTE DA TEORIA 3 --------------\n"
+preds = gnb.predict(data[used_features])
+for i in range(len(data["Name"])):
+    if preds[i]:
+        print data["Name"][i]," - Sobrevive"
+    else:
+        print data["Name"][i]," - Não sobrevive"
 
-print gnb.predict(data[used_features])
+
